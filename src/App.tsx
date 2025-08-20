1f18d7869ed8f1467d6ca654d93c4515bba7acb5
Mathys NOURRY <mathys.nourry@gmail.com>
2025-08-20 23:51:08 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './core/router';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import Competence1 from './pages/Competence1';
+import Competence2 from './pages/Competence2';
+import Competence6 from './pages/Competence6';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="competence1" element={<Competence1 />} />
+          <Route path="competence2" element={<Competence2 />} />
+          <Route path="competence6" element={<Competence6 />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
